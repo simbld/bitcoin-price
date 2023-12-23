8fa7a705884c6e9f8cc211b2d396a85ae5874930
sim <s_bullado51@hotmail.com>
2023-12-24 00:21:47 +0100

    Add useInterval hook to recover the price per second

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 function App() {
   const [bitcoinPrice, setBitcoinPrice] = useState(null);
 
-  useEffect(() => {
+  useInterval(() => {
     fetch("https://blockchain.info/ticker")
       .then((response) => response.json())
       .then((data) => setBitcoinPrice(data.EUR.last))
       .catch((error) =>
         console.error("Erreur lors de la récupération des données:", error)
       );
-  }, []);
-
+  }, [1000]);
+  console.log("Prix actualisé");
   return (
     <div>
       <h1>Prix du Bitcoin</h1>
