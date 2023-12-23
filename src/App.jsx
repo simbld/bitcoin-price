cdb53cc73841970238fc5ba5d10a1f98cbcdeabf
sim <s_bullado51@hotmail.com>
2023-12-23 23:57:11 +0100

    Add the AJAX request

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [bitcoinPrice, setBitcoinPrice] = useState(null);
 
-  return;
+  useEffect(() => {
+    fetch("https://blockchain.info/ticker")
+      .then((response) => response.json())
+      .then((data) => setBitcoinPrice(data.EUR.last))
+      .catch((error) =>
+        console.error("Erreur lors de la récupération des données:", error)
+      );
+  }, []);
+
+  return (
+    <div>
+      <h1>Prix du Bitcoin</h1>
+      {bitcoinPrice ? <p>{bitcoinPrice} EUR</p> : <p>Chargement...</p>}
+    </div>
+  );
 }
 
 export default App;
